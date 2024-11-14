9a36dd37e79f632194c2d313e6f4342a5f9f9a48
Estifanos58 <122863605+Estifanos58@users.noreply.github.com>
2024-11-14 19:55:26 +0300

    authController

---
+++ controllers/authController.js
@@ -0,0 +1,83 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/Users');
+const bcrypt = require('bcrypt');
+
+const login = async(req, res) =>{
+    const {username, password} = req.body;
+    if(!username || !password) {
+        res.status(401).json({message: "All field are Required"});
+    }
+
+    const foundUser = await User.findOne({username}).exec();
+    if(!foundUser){
+        res.status(400).json({message: "Username not Found"});
+    }
+
+    const match = await bcrypt.compare(password, foundUser.password);
+    if(!match){
+        res.status(400).json({message: "Wrong password"});
+    }
+
+    const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": foundUser.username
+            }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: '10s'}
+    )
+
+    const refreshToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": foundUser.username
+            }
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: '20s'}
+    ) 
+
+    res.cookie('jwt', refreshToken,{
+        httpOnly: true, //accessible only by web server 
+        secure: true, //https
+        sameSite: 'None', //cross-site cookie 
+        maxAge: 7 * 24 * 60 * 60 * 1000 
+    })
+
+    res.json({accessToken})
+}
+
+const refresh = (req, res) => {
+    const cookies = req.cookies;
+
+    if(!cookies?.jwt){
+        res.status(400).json({message: "Forbiden"});
+    }
+
+    const refreshToken = cookies.jwt;
+
+    jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET,
+        async (err, decoded) =>{
+            if(err) res.status(400).json({message: "Invalid"});
+            const foundUser = await User.findOne({ username: decoded.username });
+            if(!foundUser) res.status(400).json({message: "Invalid"});
+
+            const accessToken = jwt.sign(
+                {
+                    "UserInfo":{
+                        "username": foundUser.username
+                    }
+                },
+                process.env.ACCESS_TOKEN_SECRET,
+                {expiresIn: '10s'}
+            )
+
+            res.json({accessToken});
+        }
+    )
+}
+
+module.exports = {login, refresh};
